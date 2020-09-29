b64101ded824b363a9ca037e004a3a430d881513
Riordan Dervin Alfredo <riordan.alfredo@monash.edu>
2020-09-29 14:03:12 +1000

    Add accept all installation in requirements.sh

--- requirements/requirements.sh
+++ requirements/requirements.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-conda install pytorch==1.4.0 torchvision==0.5.0 cudatoolkit=10.1 -c pytorch
-conda install pandas tqdm
+conda install -y pytorch==1.4.0 torchvision==0.5.0 cudatoolkit=10.1 -c pytorch
+conda install -y pandas tqdm
 pip install torch-scatter==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.4.0.html
 pip install torch-sparse==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.4.0.html
 pip install torch-cluster==latest+cu101-f https://pytorch-geometric.com/whl/torch-1.4.0.html
